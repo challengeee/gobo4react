d390869eaa7503bf677e56597f7d3a227b00ab25
ReiRev <reirev2913@gmail.com>
2024-05-14 14:26:13 +0000

    fix boardState

--- stories/Gobo.stories.tsx
+++ stories/Gobo.stories.tsx
@@ -13,19 +13,10 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
+export const AllBlack: Story = {
   args: {
     boardWidth: 500,
     boardSize: 19,
+    boardState: Array(19).fill(Array(19).fill('black')),
   },
 }
-
-// const Hoge = ()=>{
-//     return (
-//         <Gobo GoboWidth={400}>hoge</Gobo>
-//     )
-// }
-
-// export const Hoge: Story = {
-//     render: (args) => <Hoge />,
-// }
