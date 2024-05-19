0f95ab7188086c458fe63efa3ddf6a30cc79f9aa
ReiRev <reirev2913@gmail.com>
2024-05-19 13:38:11 +0000

    fix stop-color and id

--- src/components/stones/WikiWhiteStone.tsx
+++ src/components/stones/WikiWhiteStone.tsx
@@ -3,6 +3,7 @@ import { RenderItem } from '../../hooks/useBoardState'
 // from https://en.m.wikipedia.org/wiki/File:Go_w1.svg
 export const WikiWhiteStone: RenderItem = (boardDimensions) => {
   const { stoneRadius } = boardDimensions
+  const id = "wiki-white-stone-gradient"
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -10,13 +11,13 @@ export const WikiWhiteStone: RenderItem = (boardDimensions) => {
       height={boardDimensions.verticalLineSpacing}
     >
       <defs>
-        <radialGradient r=".48" cx=".47" id="rg" cy=".49">
-          <stop offset=".7" stop-color="#FFF" />
-          <stop offset=".9" stop-color="#DDD" />
-          <stop offset="1" stop-color="#777" />
+        <radialGradient r=".48" cx=".47" id={id} cy=".49">
+          <stop offset=".7" stopColor="#FFF" />
+          <stop offset=".9" stopColor="#DDD" />
+          <stop offset="1" stopColor="#777" />
         </radialGradient>
       </defs>
-      <circle fill="url(#rg)" r={stoneRadius} cx="50%" cy="50%" />
+      <circle fill={`url(#${id})`} r={stoneRadius} cx="50%" cy="50%" />
     </svg>
   )
 }
