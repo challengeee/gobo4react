eb66c8e81ad404bc48179a8488c70665fb893f10
ReiRev <reirev2913@gmail.com>
2024-05-19 09:08:07 +0000

    support Chinese board make it default

--- stories/GoboWithRandomButton.tsx
+++ stories/GoboWithRandomButton.tsx
@@ -7,11 +7,13 @@ import { WhiteStone } from '../src/components/WhiteStone'
 interface GoboWithRandomButtonProps {
   boardWidth: number
   boardSize: number
+  boardType?: 'Chinese' | 'Japanese'
 }
 
 export const GoboWithRandomButton = ({
   boardWidth,
   boardSize,
+  boardType = 'Chinese',
 }: GoboWithRandomButtonProps) => {
   const { boardState, addStone } = useBoardState(boardSize)
   return (
@@ -41,6 +43,7 @@ export const GoboWithRandomButton = ({
       <Gobo
         boardSize={boardSize}
         boardWidth={boardWidth}
+        boardType={boardType}
         boardState={boardState}
       />
     </div>
