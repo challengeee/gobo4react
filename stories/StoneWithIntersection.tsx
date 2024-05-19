e70639569abf1b1c00256483c70d0ed3bc0014c8
ReiRev <reirev2913@gmail.com>
2024-05-19 11:23:56 +0000

    create StoneWithIntersection for debug

---
+++ stories/StoneWithIntersection.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Intersection } from '../src/components/Intersection'
+import { BlackStone, WhiteStone, WikiBlackStone } from '../src/components/stones'
+import { useBoardDimensions } from '../src/hooks'
+
+interface StoneWithIntersectionProps {
+  isTopMost?: boolean
+  isBottomMost?: boolean
+  isLeftMost?: boolean
+  isRightMost?: boolean
+  boardWidth: number
+  isStarPoint?: boolean
+  stoneType: 'black' | 'white' | 'wikiBlack'
+  boardType: 'Chinese' | 'Japanese'
+}
+
+export const StoneWithIntersection = ({
+  isTopMost,
+  isBottomMost,
+  isLeftMost,
+  isRightMost,
+  boardWidth,
+  isStarPoint,
+  stoneType,
+  boardType = 'Chinese',
+}: StoneWithIntersectionProps) => {
+  const boardDimensions = useBoardDimensions(boardWidth, 19, boardType)
+
+  const Stone = (stoneType) => {
+    switch (stoneType) {
+      case 'black':
+        return BlackStone(boardDimensions)
+      case 'white':
+        return WhiteStone(boardDimensions)
+      case 'wikiBlack':
+        return WikiBlackStone(boardDimensions)
+      default:
+        return BlackStone(boardDimensions)
+    }
+  }
+
+  return (
+    <div
+      style={{
+        width: boardDimensions.horizontalLineSpacing,
+        height: boardDimensions.verticalLineSpacing,
+        backgroundColor: 'green',
+      }}
+    >
+      <Intersection
+        isTopMost={isTopMost}
+        isBottomMost={isBottomMost}
+        isLeftMost={isLeftMost}
+        isRightMost={isRightMost}
+        width={boardDimensions.horizontalLineSpacing}
+        height={boardDimensions.verticalLineSpacing}
+        lineThickness={boardDimensions.lineThickness}
+        isStarPoint={isStarPoint}
+        starPointMarkerRadius={boardDimensions.starPointMarkerRadius}
+      >
+        {Stone(stoneType)}
+      </Intersection>
+    </div>
+  )
+}
