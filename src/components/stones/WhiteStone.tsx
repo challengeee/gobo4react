fd1821079e2aecf63b392fd9a6d6c25617d92f3c
ReiRev <reirev2913@gmail.com>
2024-06-16 11:22:25 +0000

    add borderSize

--- src/components/stones/WhiteStone.tsx
+++ src/components/stones/WhiteStone.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
+import { BoardDimensions } from '../../hooks/useBoardDimensions'
 import { RenderStone } from '../../hooks/useBoardState'
 
-export const WhiteStone: RenderStone = ({ stoneRadius }) => {
+export const WhiteStone: RenderStone = ({
+  stoneRadius,
+  lineThickness,
+  borderSize,
+}: BoardDimensions & { borderSize?: number }) => {
   return (
     <svg>
-      <circle cx="50%" cy="50%" r={stoneRadius} fill="white" />
+      <circle
+        cx="50%"
+        cy="50%"
+        r={stoneRadius}
+        fill="white"
+        stroke="black"
+        strokeWidth={borderSize || lineThickness / 2}
+      />
     </svg>
   )
 }
